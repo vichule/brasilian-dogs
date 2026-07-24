63d2fe0181784bfcd49a9ba67da59702576947a5
vichule <javier.cabanasdiaz@gmail.com>
2026-07-24 11:11:40 +0200

    Add setup css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-import Hero from "@/sections/Hero";
+import Hero from "@/sections/Hero/Hero";
 import Services from "@/sections/Services";
 import About from "@/sections/About";
 import Gallery from "@/sections/Gallery";
