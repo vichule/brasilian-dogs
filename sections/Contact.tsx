5f635eb80e97955019187105dab0e197701ed268
vichule <javier.cabanasdiaz@gmail.com>
2026-07-24 11:13:39 +0200

    Add web sections estructure

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <section>
+        <section id="contact">
             Contact
         </section>
     );
--- sections/Services.tsx
+++ sections/Services.tsx
@@ -1,6 +1,6 @@
 export default function Services() {
     return (
-        <section>
+        <section id="services">
             Services
         </section>
     );
